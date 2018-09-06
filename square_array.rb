fa4b8dbcf860558e9e58df2aade4592478a19ef2
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-06 23:35:49 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  array.collect { |num| num**2 }
+  sq_array = []
+  array.each {|num| sq_array << num**2}
+  sq_array
+end
+
+def square_array_with_collect(array)
+  array.collect{|num| num**2}
 end
